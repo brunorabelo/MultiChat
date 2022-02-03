af28064039c9714918ab762ebfc4d55c9b6ace46
Bruno Botelho <brunorabelow@gmail.com>
2022-02-03 18:57:32 +0100

    daphne5

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -35,7 +35,7 @@ if [ "$DATABASE" = "postgres" ]; then
   python manage.py collectstatic --no-input --clear
 
   #  daphne -p 8001 multichat.asgi:application -b 0.0.0.0
-  daphne -u /home/app/web/tmp/daphne.sock -p 8001 multichat.asgi:application
+  daphne -p 8001 multichat.asgi:application -b 0.0.0.0
 
 fi
 
